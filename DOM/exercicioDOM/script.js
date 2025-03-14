b87e71df96a72072b7a479b193f028823cae4e7b
derick-gomes <derickpizza@gmail.com>
2025-03-14 08:21:38 -0300

    14/03/2025

--- DOM/exercicioDOM/script.js
+++ DOM/exercicioDOM/script.js
@@ -1,28 +1,21 @@
-// parte 1 - Exibir no console
-
-let titulo = document.getElementById("titulo");
-let paragrafo = document.querySelector(".paragrafo");
-
-console.log(titulo);
-console.log(paragrafo);
-
-//parte 2 - modificar elemento
-
-function mudartexto() {
-    titulo.innerText = "novo titulo";
-    paragrafo.innerText = "novo Paragrafo";
-}
-
-//parte 3 - modificar e estilo
-
-function mudarfundo() {
-    let body = document.querySelector("body");
-    body.style.backgroundColor = "blue";
-}
-
-// parte 4 - Adicionar um classe de elementos
-
-function adicionarClasse() {
-    titulo.classList.add("descricao");
-    document.querySelector(".descricao").style.color = "red"
-}
+// Exemplo de uso de DOM
+//header => DOM
+let header = document.createElement("div");
+document.body.appendChild(header);
+header.style.backgroundColor = "black";
+header.style.height = "8vh"
+let menu = document.createElement("div");
+header.appendChild(menu);
+header.classList.add("header");
+menu.classList.add("menu");
+let menuItens = ["Inicio","produtos", "contato"];
+menuItens.forEach(element => {
+    let item = document.createElement("a");
+    item.innerText = element;
+    menu.appendChild(item);
+});
+menu.style.display = "flex";
+menu.style.justifyContent = "space-around";
+menu.style.alignItems = "center"
+menu.style.color = "white"
+menu.style.height = "100%"
