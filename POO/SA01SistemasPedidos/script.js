720372b102922912f108ae0e81e50580090e91b2
derick-gomes <derickpizza@gmail.com>
2025-04-04 11:28:20 -0300

    04/04/2025

---
+++ POO/SA01SistemasPedidos/script.js
@@ -0,0 +1,70 @@
+//criar as classes
+
+class Cliente{
+    constructor(id, nome){
+        this.id = id;
+        this.nome = nome;
+
+    }
+}
+
+class Produto{
+    constructor(id, nome, preco){
+        this.id = id;
+        this.nome = nome;
+        this.preco;
+    }
+}
+
+class Pedido{
+    constructor(id, cliente, itens, desconto){
+        this.id = id;
+        this.cliente = cliente;
+        this.itens = itens;
+        this.desconto = desconto;
+        this.total = this.calcularTotal();
+
+    }
+    calcularTotal(){
+        let total = this.reduce((acc,item)=>
+        acc+(item.Produto.preco * item.quantidade),0);
+        return total - (total*(this.desconto/100));
+
+    }
+}
+
+class SistemaPedidos{
+    constructor(){
+        this.clientes =[];
+        this.Produtos =[];
+        this.Pedidos = [];
+    }
+
+    cadastrarCliente(){
+        const nome = document.getElementById("clienteNome").value;
+        if(!nome) return alert ("Digite um Nome Para o Cliente.");
+        const cliente = new Cliente(this.clientes.length+1,nome)
+        this.clientes.push(cliente);
+    }
+
+    cadastrarProduto(){
+        const nome = document.getElementById("produtoNome").value;
+        const preco = parseFloat(document.getElementById("produtoPreco").value);
+        if(!nome || !preco) return alert("Preencha Todos os Campos do Produto");
+        const produto = new Produto(this.produtos.length+1,nome,preco);
+        this.produtos.push(produto);
+    }
+
+    atualizarClientes(){
+        const lista = document.getElementById("listaClientes");
+        lista.innerHTML = "";
+
+        const selectCliente = document.getElementById("selectCliente");
+        selectCliente.innerHTML = '<option value = "">Selecione um Cliente</option>';
+        this.clientes.forEach( cliente => {
+            const li = document.createElement("li");
+            li.innerText - cliente.nome;
+        })
+    }
+
+}
